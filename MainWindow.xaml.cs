2cc2b02cd59ee88a6511ff9fc73a85204ba0032a
Paul B Colburn <paulcolburn@outlook.com>
2026-04-20 16:57:44 -0500

    added Window_Loaded

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -180,5 +180,25 @@ namespace RockPaperScissors
                 synth.SpeakAsyncCancelAll();
             }
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            double screenWidth = SystemParameters.WorkArea.Width;
+            double screenHeight = SystemParameters.WorkArea.Height;
+
+            // ensure window size doesn't exceed screen size
+            if (this.Width > screenWidth) this.Width = screenWidth;
+            if (this.Height > screenHeight) this.Height = screenHeight;
+
+            // ensure window is not off the left or top
+            if (this.Left < 0) this.Left = 0;
+            if (this.Top < 0) this.Top = 0;
+
+            // ensure window is not off the right or bottom
+            if (this.Left + this.Width > screenWidth)
+                this.Left = screenWidth - this.Width;
+            if (this.Top + this.Height > screenHeight)
+                this.Top = screenHeight - this.Height;
+        }
     }
 }
